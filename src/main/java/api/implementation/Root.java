fc3f3b6fc05325f5c634e5c20e531172535021e5
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2023-01-23 09:41:54 +0000

    create interface of "root" && add javadoc comments

--- src/main/java/api/implementation/Root.java
+++ src/main/java/api/implementation/Root.java
@@ -1,11 +1,80 @@
 package api.implementation;
 
+import api.interfaces.IRoot;
+
 import java.util.ArrayList;
 
-public class Root
+/**
+ * Representacao da classe da raiz do JSON
+ */
+public class Root implements IRoot
 {
-    public ArrayList<Local> locals;
-    public ArrayList<Route> routes;
-    public ArrayList<Player> players;
-    public ArrayList<GlobalGameSetting> globalGameSettings;
+    /**
+     * locais (portal e connector)
+     */
+    private ArrayList<Local> locals;
+
+    /**
+     * rotas
+     */
+    private ArrayList<Route> routes;
+
+    /**
+     * jogadores
+     */
+    private ArrayList<Player> players;
+
+    /**
+     * definicoes globais do jogo
+     */
+    private ArrayList<GlobalGameSetting> globalGameSettings;
+
+
+    @Override
+    public ArrayList<Local> getLocals()
+    {
+        return locals;
+    }
+
+    @Override
+    public void setLocals(ArrayList<Local> locals)
+    {
+        this.locals = locals;
+    }
+
+    @Override
+    public ArrayList<Route> getRoutes()
+    {
+        return routes;
+    }
+
+    @Override
+    public void setRoutes(ArrayList<Route> routes)
+    {
+        this.routes = routes;
+    }
+
+    @Override
+    public ArrayList<Player> getPlayers()
+    {
+        return players;
+    }
+
+    @Override
+    public void setPlayers(ArrayList<Player> players)
+    {
+        this.players = players;
+    }
+
+    @Override
+    public ArrayList<GlobalGameSetting> getGlobalGameSettings()
+    {
+        return globalGameSettings;
+    }
+
+    @Override
+    public void setGlobalGameSettings(ArrayList<GlobalGameSetting> globalGameSettings)
+    {
+        this.globalGameSettings = globalGameSettings;
+    }
 }
