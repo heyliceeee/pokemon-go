e906ebde608dc725b8459b222a3d7e5283e6b867
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2023-01-24 12:38:47 +0000

    limpar projeto (porque está a dar erros)

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -6,14 +6,14 @@ public class Demo
 {
     static String value = "Hello World!";
 
+    /**
+     * Mostra o menu inicial
+     */
     public static void showMenu()
     {
         System.out.println("\n");
-        System.out.println("+--------------------------------------+");
-        System.out.println("|                 MENU                 |");
-        System.out.println("+--------------------------------------+");
         System.out.println(
-                "| 01. a |\n" +
+                "| 01. teste |\n" +
                         "| 02. b |\n" +
                         "| 03. c |\n" +
                         "| 04. d |\n" +
@@ -24,11 +24,34 @@ public class Demo
         System.out.println("+--------------------------------------+\n\n");
     }
 
+
+    /**
+     * Depois de selecionar a opcao do menu inicial, faz o que e pretendido
+     * @param option e a opcao selecionada do menu inicial
+     */
+    public static void chooseOption(int option)
+    {
+        switch (option)
+        {
+            case 1:
+                System.out.println("selected option 1");
+                break;
+
+            case 12:
+                break;
+            default:
+        }
+    }
+
     public static void main(String[] args)
     {
         int option=0;
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("+--------------------------------------+");
+        System.out.println("|                 MENU                 |");
+        System.out.println("+--------------------------------------+");
 
         do
         {
@@ -41,6 +64,9 @@ public class Demo
             {
                 System.out.println("ERROR: "+e.getMessage());
             }
+
+            chooseOption(option);
+
         } while (option != 99);
     }
 
