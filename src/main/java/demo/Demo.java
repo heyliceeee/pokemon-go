89ce14e751e665f8e8846389ff1b7b19e587ab12
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2023-01-25 19:58:56 +0000

    menu da gestão do jogo, menu da gestão de jogadores e menu de gestão de rotas

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -9,8 +9,7 @@ public class Demo {
     /**
      * Mostra o menu inicial
      */
-    public static void showMainMenu()
-    {
+    public static void showMainMenu() {
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
         int option = 0;
@@ -21,7 +20,7 @@ public class Demo {
             System.out.println("|                 MENU                 |");
             System.out.println("+--------------------------------------+");
             System.out.println(
-                            "| 01. Game                             |\n" +
+                    "| 01. Game                             |\n" +
                             "| 02. API                              |\n" +
                             "| 99. Exit                             |"
             );
@@ -54,10 +53,8 @@ public class Demo {
         }
     }
 
-    private static void showGameMenu()
-    {
+    private static void showGameMenu() {
     }
-
 
     /**
      * Mostra o menu acerca da API
@@ -73,7 +70,7 @@ public class Demo {
             System.out.println("|               API MENU               |");
             System.out.println("+--------------------------------------+");
             System.out.println(
-                            "| 01. Import JSON file                 |\n" +
+                    "| 01. Import JSON file                 |\n" +
                             "| 02. Portals management               |\n" +
                             "| 03. Connectors management            |\n" +
                             "| 04. Routes management                |\n" +
@@ -130,13 +127,179 @@ public class Demo {
         }
     }
 
-    private static void showGameManagementMenu() {
+    /**
+     * Mostra o menu acerca do jogo
+     */
+    private static void showGameManagementMenu()
+    {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        int option = 0;
+
+        while (!exit) {
+            System.out.println("\n");
+            System.out.println("+--------------------------------------------------------------------------------+");
+            System.out.println("|                              GAME MANAGEMENT MENU                              |");
+            System.out.println("+--------------------------------------------------------------------------------+");
+            System.out.println(
+                               "| 01. Calculate the shortest path between 2 points                               |\n" +
+                               "| 02. Calculate the shortest path if going through places to recharge            |\n" +
+                               "| 03. Calculate the shortest path if passing through portals and connectors only |\n" +
+                               "| 04. Export each portal/connector involved                                      |\n" +
+                               "| 05. Import game settings                                                       |\n" +
+                               "| 06. Export game settings                                                       |\n" +
+                               "| 99. Back to previous menu                                                      |"
+            );
+            System.out.println("+--------------------------------------------------------------------------------+\n\n");
+
+            System.out.println("select an option: ");
+            option = scanner.nextInt();
+
+            /**
+             * Depois de selecionar a opcao do menu, faz o que pretende
+             */
+            switch (option) {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 99:
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("invalid option, selected option between 1 and 8 or 99 to exit.");
+                    break;
+            }
+        }
     }
 
+    /**
+     * Mostra o menu da gestao dos jogadores
+     */
     private static void showPlayersManagementMenu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        int option = 0;
+
+        while (!exit) {
+            System.out.println("\n");
+            System.out.println("+--------------------------------------------------------+");
+            System.out.println("|                 PLAYERS MANAGEMENT MENU                |");
+            System.out.println("+--------------------------------------------------------+");
+            System.out.println(
+                               "| 01. Add                                                |\n" +
+                               "| 02. Update                                             |\n" +
+                               "| 03. Delete                                             |\n" +
+                               "| 04. Join the team                                      |\n" +
+                               "| 05. Leave the team                                     |\n" +
+                               "| 06. List (by team, level, number of conquered portals) |\n" +
+                               "| 07. Import statistics                                  |\n" +
+                               "| 08. Export statistics                                  |\n" +
+                               "| 99. Back to previous menu                              |"
+            );
+            System.out.println("+--------------------------------------------------------+\n\n");
+
+            System.out.println("select an option: ");
+            option = scanner.nextInt();
+
+            /**
+             * Depois de selecionar a opcao do menu, faz o que pretende
+             */
+            switch (option) {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 99:
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("invalid option, selected option between 1 and 8 or 99 to exit.");
+                    break;
+            }
+        }
     }
 
+    /**
+     * Mostra o menu acerca da gestao das rotas
+     */
     private static void showRoutesManagementMenu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        int option = 0;
+
+        while (!exit) {
+            System.out.println("\n");
+            System.out.println("+--------------------------------------+");
+            System.out.println("|        ROUTES MANAGEMENT MENU        |");
+            System.out.println("+--------------------------------------+");
+            System.out.println(
+                    "| 01. Create                           |\n" +
+                            "| 02. Delete                           |\n" +
+                            "| 03. Import portals/connectors        |\n" +
+                            "| 04. Export portals/connectors        |\n" +
+                            "| 99. Back to previous menu            |"
+            );
+            System.out.println("+--------------------------------------+\n\n");
+
+            System.out.println("select an option: ");
+            option = scanner.nextInt();
+
+            /**
+             * Depois de selecionar a opcao do menu, faz o que pretende
+             */
+            switch (option) {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 99:
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("invalid option, selected option between 1 and 7 or 99 to exit.");
+                    break;
+            }
+        }
     }
 
     /**
@@ -153,7 +316,7 @@ public class Demo {
             System.out.println("|        PORTALS MANAGEMENT MENU       |");
             System.out.println("+--------------------------------------+");
             System.out.println(
-                            "| 01. Add                              |\n" +
+                    "| 01. Add                              |\n" +
                             "| 02. Edit                             |\n" +
                             "| 03. Delete                           |\n" +
                             "| 04. List                             |\n" +
@@ -206,8 +369,7 @@ public class Demo {
     /**
      * Mostra o menu acerca da gestao dos conectores
      */
-    private static void showConnectorsManagementMenu()
-    {
+    private static void showConnectorsManagementMenu() {
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
         int option = 0;
@@ -218,7 +380,7 @@ public class Demo {
             System.out.println("|      CONNECTORS MANAGEMENT MENU      |");
             System.out.println("+--------------------------------------+");
             System.out.println(
-                            "| 01. Add                              |\n" +
+                    "| 01. Add                              |\n" +
                             "| 02. Edit                             |\n" +
                             "| 03. Delete                           |\n" +
                             "| 04. Add interactions                 |\n" +
@@ -241,7 +403,6 @@ public class Demo {
                     break;
 
                 case 2:
-
                     break;
 
                 case 3:
@@ -267,7 +428,7 @@ public class Demo {
                     break;
 
                 default:
-                    System.out.println("invalid option, selected option between 1 and 7 or 99 to exit.");
+                    System.out.println("invalid option, selected option between 1 and 8 or 99 to exit.");
                     break;
             }
         }
