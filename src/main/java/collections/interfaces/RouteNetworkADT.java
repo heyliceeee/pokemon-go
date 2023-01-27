953f6fd45ba01e68fa9b30f27b1ad6f9f2060397
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2023-01-27 23:31:49 +0000

    RouteNetworkADT (ainda não terminei)

--- src/main/java/collections/interfaces/RouteNetworkADT.java
+++ src/main/java/collections/interfaces/RouteNetworkADT.java
@@ -1,10 +1,41 @@
 package collections.interfaces;
 
+import api.interfaces.IConnector;
+import api.interfaces.IPortal;
+import api.interfaces.IRoute;
+
+import java.util.Iterator;
+
 /**
  * Contrato de um gráfico de caminho no campo de caminhos a jogadores
  * @param <T> tipo de locais no grafo
  */
 public interface RouteNetworkADT<T> extends NetworkADT<T>
 {
+    /**
+     * Retorna o número {@link api.implementation.Portal portals} do grafo
+     * @return o número {@link api.implementation.Portal portals} do grafo
+     */
+    int getNumberOfPortals();
 
+    /**
+     * Retorna o número {@link api.implementation.Connector conectores} do grafo
+     * @return o número {@link api.implementation.Connector conectores} do grafo
+     */
+    int getNumberOfConnectors();
+
+    /**
+     * Retorna os portais do grafo
+     * @return iterador com portais
+     */
+    Iterator<IPortal> getPortals();
+
+    /**
+     * Retorna os conectores do grafo
+     * @return iterador com conectores
+     */
+    Iterator<IConnector> getConnectors();
+
+
+    Iterator<IRoute>
 }
